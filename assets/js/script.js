187f02d2b03c8e420df1a2cf08bf2a6919c0be2a
Dane Cronin <danecronin@gmail.com>
2022-08-06 12:31:39 -0600

    added psuedocode for basic structure/layout of the coding quiz logic

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// create variables to be used for multiple choice quiz:
+
+
+
+
+//Setup a click event / function to start a timer when the "Start Quiz!" button is clicked:
+
+    // In the click event handler function make the heading "Coding Quiz!" disappear
+
+    // Show the first question and set of answers 
+
+
+
+
+// Setup a questions function to display question with answer choices.
+
+    //Create 4 buttons, one for each choice in an ordered list
+
+    //Get answer choice and compare to answer
+
+         //Display Correct! if correct answert, wrong if it is the wrong answer!
+    
+        // If it is the wrong answer, deduct 10 seconds from timer
+
+        // If it is correct answer, add points and show next question and choices.
+
+
+
+// Create function to run all questions or if timer runs out, game is over.
+
+    //When all questions have been answered or timer runs out, user can save name and score
+
+
+// Create function to store and save user's name and score to High Scores list
+
+
+// Create function to load the stored high scores on high-scores.html page
+
+
