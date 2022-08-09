02236519c3b7c0c7b20846d3e4a7a461313c92bc
Dane Cronin <danecronin@gmail.com>
2022-08-09 17:42:25 -0600

    deleted all of code for first function attempt to run Quiz Questions.  Met with Tutor and worked on function to put answers in buttons and answer choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,11 @@ var hideStartQuiz = document.querySelector("#start"); // Starting screen of quiz
 var quizEl = document.querySelector("#questions"); //quiz container element
 var questionTitle = document.querySelector("#question-title") // Actual Question
 var answerButtons = document.querySelector(".answer-buttons") // Div element to hold ordered list of answer buttons
-var answerChoiceEl = document.querySelector("#answer-choice"); // Possible Answer Choice Buttons
-
-
+// var answerChoiceEl = document.querySelector("#answer-choice"); // Possible Answer Choice Buttons
+var currentIndex = 0
+    // Variable to store the score
+    var score = [];
+    var timeLeft = 75;
 
 
 
@@ -19,30 +21,30 @@ var myQuestions = [
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: [
-            { a: "Console Log" },
-            { b: "Terminal/Bash" },
-            { c: "For Loops" },
-            { d: "Javascript" },
+             "Console Log" ,
+              "Terminal/Bash" ,
+              "For Loops" ,
+             "Javascript" ,
         ],
         correctAnswer: "a"
     },
     {
         question: "Commonly used data types DO NOT include ",
         answers: [
-            { a: "Strings" },
-            { b: "Booleans" },
-            { c: "Alerts" },
-            { d: "Numbers" },
+              "Strings" ,
+             "Booleans" ,
+              "Alerts" ,
+              "Numbers" ,
         ],
         correctAnswer: "c"
     },
     {
         question: "What can be stored within Arrays in Javascript?",
         answers: [
-            { a: "Numbers and Strings" },
-            { b: "Other Arrays" },
-            { c: "Booleans" },
-            { d: "All of the Above" },
+             "Numbers and Strings" ,
+              "Other Arrays" ,
+             "Booleans" ,
+              "All of the Above" ,
         ],
         correctAnswer: "d"
     },
@@ -72,61 +74,6 @@ var startQuiz = function (event) {
 }; // End Start Quiz Function
 
 
-//     // Return Quiz - Show the first question and set of answers 
-// function quizQuestions() {
-//     console.log("quiz questions");
-
-//     //create array to store all of the output and answer choices
-//     var output = []; 
-//     //variable to store all of the answers
-//     var answers = [];  
-//     //for each question, iterate through with for loop
-
-//     for (var i = 0; i < myQuestions.length; i++) {
-
-//         //console.log("QUESTION");
-//         answers = window.prompt(myQuestions[i].question);
-
-//         if (answers == myQuestions[i].correctAnswer) {
-//             console.log("question1");
-//             score++;
-//             alert("Correct!");
-//         } else {
-//             alert("Wrong!");
-//             countdown(timeLeftEl - 10000);
-//         }
-//     };
-
-
-//     //     // for each available answer to this question...
-//     //     for(letter in myQuestions[i].answers){
-
-// 	// 		// ...add question buttons
-// 	// 		answers.push(
-// 	// 			'<label>'
-// 	// 				+ '<input type="button" name="question'+i+'" value="'+letter+'">'
-// 	// 				+ letter + ': '
-// 	// 				+ myQuestions[i].answers[letter]
-// 	// 			+ '</label>'
-// 	// 		);
-// 	// 	}
-
-// 	// 	// add this question and its answers to the output
-// 	// 	output.push(
-// 	// 		'<div class="question">' + myQuestions[i].question + '</div>'
-// 	// 		+ '<div class="answers">' + answers.join('') + '</div>'
-// 	// 	);
-// 	// }
-//     // //combine output list into one string of html and put it on the page
-// 	// quizEl.innerHTML = output.join('');
-
-//     //  quizQuestions(myQuestions, quizEl);
-
-// }; // End return quiz function
-
-
-
-
 
 // Function to start a timer when "Start Quiz!" button is clicked.
 startQuizEl.addEventListener("click", function () {
@@ -137,16 +84,15 @@ startQuizEl.addEventListener("click", function () {
 
 function countdown() {
     console.log("countdown function was called");
-    var timeLeft = 75;
+  
 
     var timeInterval = setInterval(function () {
-        timeLeftEl.innerHTML = timeLeft;
         timeLeft--;
+        timeLeftEl.innerHTML = timeLeft;
         if (timeLeft <= 0) {
             clearInterval(timeInterval);
             alert("Sorry, out of time");
         }
-
     }, 1000);
 
 };
@@ -161,35 +107,19 @@ function countdown() {
 // Setup a questions function to display question with answer choices.
 function quizQuestions() {
     console.log("Quiz Question Started");
-    // Variable to store the score
-    var score = [];
-
-    for (var i = 0; i < myQuestions.length; i++) {
-        questionTitle.innerText = myQuestions[i].question;
+    console.log(myQuestions[currentIndex])
+        answerButtons.innerHTML = ''
+        questionTitle.innerText = myQuestions[currentIndex].question;
 
         //Add possible answers to buttons to choose Answer
-        myQuestions[i].answers.forEach(answer => {
-            answerButtons = document.createElement('button');
-            answerChoiceEl.innerText = answer.text
-            answerChoiceEl.classList.add('btn')
-
-            //Get answer choice and compare to answer
-            if (answer.correct) {
-
-                // If it is correct answer, add points and show next question and choices.
-                button.dataset.correct = answer.correct
-                //  score ++;
-                // alert("Correct!");
-            } else {
-                // alert("WRONG!");
-            }
-
-            answerChoiceEl.addEventListener('click', selectAnswer);
-            answerButtons.appendChild(answerChoiceEl);
+        myQuestions[currentIndex].answers.forEach(answer => {
+            var answerButton = document.createElement('button');
+            answerButton.innerText = answer;
+            answerButton.classList.add('btn');
+            answerButton.onclick = selectAnswer
+            answerButtons.append(answerButton);
 
         });
-
-    }
 
     // If it is the wrong answer, deduct 10 seconds from timer
 
@@ -199,20 +129,37 @@ function quizQuestions() {
 
 // Create Function to clear all answers and questions to move on to next question
 
+function selectAnswer() {
+        var currentAnswer = myQuestions[currentIndex].correctAnswer
+        var clickedAnswer = this.innerText
+        console.log(`ClickedAnswer: ${clickedAnswer}
+        CorrectAnswer: ${currentAnswer}`)
+            //Get answer choice and compare to answer
+            if (currentAnswer !== clickedAnswer)  {
+                timeLeft -= 10
+                // If it is correct answer, add points and show next question and choices.
 
+                //  score ++;
+            } 
+            currentIndex++
+            if(currentIndex === myQuestions.length){
+                // end quiz Function
+            } else {
+                quizQuestions()
+            }
 
-
-function selectAnswer(event) {
-
-}
-
-
+        }
 
 // Create function to run all questions or if timer runs out, game is over.
 
     //When all questions have been answered or timer runs out, user can save name and score
 
+    var newScoreObj = {score: timeLeft , initials}
 
+    var highScores = JSON.parse(localStorage.getItems('Highscores')) || []
+
+    highScores.push(newScoreObj)
+    localStorage.setItem('Highscores', JSON.stringify(highScores))
 // Create function to store and save user's name and score to High Scores lists
 
 
